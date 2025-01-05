fb84f43f55f6d4392615096948923e92adb9ed3b
Ashvin Panicker <agnjkafgh@gmail.com>
2025-01-06 00:11:55 +0530

    update data

--- components/intro.tsx
+++ components/intro.tsx
@@ -91,7 +91,7 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/Resume_Ashvin_Panicker_2023.pdf"
+          href="/Ashvin_Panicker_Resume.pdf"
           download
         >
           Download CV{" "}
