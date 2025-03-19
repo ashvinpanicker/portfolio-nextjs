daf9f29e1b4b2cc24e73f2a75d410006dfa0948c
Ashvin Panicker <agnjkafgh@gmail.com>
2025-03-19 06:08:32 +0530

    Init circle pack

---
+++ components/skills-circle-pack.tsx
@@ -0,0 +1,39 @@
+import React, { FC, useState } from 'react';
+import { CirclePacking } from '@nivo/circle-packing';
+import { skillsData } from '../lib/skills-circle-pack-data';
+
+const commonProperties = {
+    width: 1000,
+    height: 550,
+    data: skillsData,
+    padding: 2,
+    id: 'name',
+    value: 'loc',
+    labelsSkipRadius: 16,
+}
+
+// TODO Fix theme colors
+// TODO Fix label colors
+// TODO Cursor pointer on hover
+// TODO add instructions on how to zoom in and out
+// TODO Change Data
+export const SkillsCirclePack: FC = () => {
+    const [zoomedId, setZoomedId] = useState<string | null>(null);
+    return (
+        <CirclePacking
+            {...commonProperties}
+            enableLabels
+            labelsSkipRadius={16}
+            labelsFilter={label => label.node.height === 0}
+            labelTextColor={{
+                from: 'color',
+                modifiers: [['darker', 2]],
+            }}
+            zoomedId={zoomedId}
+            motionConfig="slow"
+            onClick={node => {
+                setZoomedId(zoomedId === node.id ? null : node.id)
+            }}
+        />
+    );
+};
--- components/skills.tsx
+++ components/skills.tsx
@@ -5,6 +5,7 @@ import SectionHeading from "./section-heading";
 import { skillsData } from "@/lib/data";
 import { useSectionInView } from "@/lib/hooks";
 import { motion } from "framer-motion";
+import { SkillsCirclePack } from "./skills-circle-pack";
 
 const fadeInAnimationVariants = {
   initial: {
@@ -27,26 +28,36 @@ export default function Skills() {
     <section
       id="skills"
       ref={ref}
-      className="mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40"
+      className="mb-28 scroll-mt-28 text-center sm:mb-40"
     >
       <SectionHeading>My skills</SectionHeading>
-      <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
-        {skillsData.map((skill, index) => (
-          <motion.li
-            className="bg-white borderBlack rounded-xl px-5 py-3 dark:bg-white/10 dark:text-white/80"
-            key={index}
-            variants={fadeInAnimationVariants}
-            initial="initial"
-            whileInView="animate"
-            viewport={{
-              once: true,
-            }}
-            custom={index}
-          >
-            {skill}
-          </motion.li>
-        ))}
-      </ul>
+      <SkillsCirclePack />
     </section>
   );
+  // return (
+  //   <section
+  //     id="skills"
+  //     ref={ref}
+  //     className="mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40"
+  //   >
+  //     <SectionHeading>My skills</SectionHeading>
+  //     <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
+  //       {skillsData.map((skill, index) => (
+  //         <motion.li
+  //           className="bg-white borderBlack rounded-xl px-5 py-3 dark:bg-white/10 dark:text-white/80"
+  //           key={index}
+  //           variants={fadeInAnimationVariants}
+  //           initial="initial"
+  //           whileInView="animate"
+  //           viewport={{
+  //             once: true,
+  //           }}
+  //           custom={index}
+  //         >
+  //           {skill}
+  //         </motion.li>
+  //       ))}
+  //     </ul>
+  //   </section>
+  // );
 }
