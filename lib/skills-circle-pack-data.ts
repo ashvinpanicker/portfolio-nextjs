bf3f956bfb5afcb3fa5b3c61e02c1dbeabc290ca
Ashvin Panicker <ashvin.panicker@gmail.com>
2025-03-25 04:03:42 +0530

    change skill data

--- lib/skills-circle-pack-data.ts
+++ lib/skills-circle-pack-data.ts
@@ -3,418 +3,149 @@ export const skillsData = {
   color: "hsl(188, 70%, 50%)",
   children: [
     {
-      name: "Frontend",
+      name: "Frontend Development",
       color: "hsl(316, 70%, 50%)",
       children: [
         {
-          name: "stack",
+          name: "Core Skills",
           color: "hsl(109, 70%, 50%)",
           children: [
-            {
-              name: "cchart",
-              color: "hsl(200, 70%, 50%)",
-              loc: 167912,
-            },
-            {
-              name: "xAxis",
-              color: "hsl(133, 70%, 50%)",
-              loc: 129793,
-            },
-            {
-              name: "yAxis",
-              color: "hsl(297, 70%, 50%)",
-              loc: 166729,
-            },
-            {
-              name: "layers",
-              color: "hsl(302, 70%, 50%)",
-              loc: 22227,
-            },
+            { name: "HTML5", color: "hsl(109, 70%, 50%)", loc: 90 },
+            { name: "CSS3", color: "hsl(200, 70%, 50%)", loc: 85 },
+            { name: "JavaScript", color: "hsl(133, 70%, 50%)", loc: 95 },
+            { name: "TypeScript", color: "hsl(297, 70%, 50%)", loc: 88 },
           ],
         },
         {
-          name: "ppie",
+          name: "Frameworks & Libraries",
           color: "hsl(222, 70%, 50%)",
           children: [
+            { name: "React.js", color: "hsl(61, 70%, 50%)", loc: 92 },
+            { name: "Next.js", color: "hsl(252, 70%, 50%)", loc: 89 },
             {
-              name: "chart",
-              color: "hsl(61, 70%, 50%)",
-              children: [
-                {
-                  name: "pie",
-                  color: "hsl(252, 70%, 50%)",
-                  children: [
-                    {
-                      name: "outline",
-                      color: "hsl(282, 70%, 50%)",
-                      loc: 96185,
-                    },
-                    {
-                      name: "slices",
-                      color: "hsl(147, 70%, 50%)",
-                      loc: 73182,
-                    },
-                    {
-                      name: "bbox",
-                      color: "hsl(271, 70%, 50%)",
-                      loc: 185270,
-                    },
-                  ],
-                },
-                {
-                  name: "donut",
-                  color: "hsl(323, 70%, 50%)",
-                  loc: 58752,
-                },
-                {
-                  name: "gauge",
-                  color: "hsl(15, 70%, 50%)",
-                  loc: 15371,
-                },
-              ],
+              name: "Redux / Redux-Saga",
+              color: "hsl(323, 70%, 50%)",
+              loc: 87,
             },
-            {
-              name: "legends",
-              color: "hsl(252, 70%, 50%)",
-              loc: 130831,
-            },
+            { name: "Zustand", color: "hsl(15, 70%, 50%)", loc: 80 },
+          ],
+        },
+        {
+          name: "Styling & Components",
+          color: "hsl(327, 70%, 50%)",
+          children: [
+            { name: "Tailwind CSS", color: "hsl(314, 70%, 50%)", loc: 90 },
+            { name: "Material-UI", color: "hsl(71, 70%, 50%)", loc: 85 },
+            { name: "Bootstrap", color: "hsl(92, 70%, 50%)", loc: 80 },
+            { name: "ant-design", color: "hsl(67, 70%, 50%)", loc: 78 },
+            { name: "Storybook", color: "hsl(37, 70%, 50%)", loc: 82 },
           ],
         },
       ],
     },
     {
-      name: "Design",
-      color: "hsl(327, 70%, 50%)",
-      children: [
-        {
-          name: "rgb",
-          color: "hsl(314, 70%, 50%)",
-          loc: 53584,
-        },
-        {
-          name: "hsl",
-          color: "hsl(71, 70%, 50%)",
-          loc: 83891,
-        },
-      ],
-    },
-    {
-      name: "Backend",
-      color: "hsl(92, 70%, 50%)",
-      children: [
-        {
-          name: "randomize",
-          color: "hsl(67, 70%, 50%)",
-          loc: 42702,
-        },
-        {
-          name: "resetClock",
-          color: "hsl(37, 70%, 50%)",
-          loc: 47674,
-        },
-        {
-          name: "noop",
-          color: "hsl(83, 70%, 50%)",
-          loc: 192669,
-        },
-        {
-          name: "tick",
-          color: "hsl(50, 70%, 50%)",
-          loc: 17737,
-        },
-        {
-          name: "forceGC",
-          color: "hsl(281, 70%, 50%)",
-          loc: 78324,
-        },
-        {
-          name: "stackTrace",
-          color: "hsl(173, 70%, 50%)",
-          loc: 194872,
-        },
-        {
-          name: "dbg",
-          color: "hsl(194, 70%, 50%)",
-          loc: 9590,
-        },
-      ],
-    },
-    {
-      name: "DevOps",
-      color: "hsl(139, 70%, 50%)",
-      children: [
-        {
-          name: "address",
-          color: "hsl(35, 70%, 50%)",
-          loc: 41321,
-        },
-        {
-          name: "city",
-          color: "hsl(255, 70%, 50%)",
-          loc: 7600,
-        },
-        {
-          name: "animal",
-          color: "hsl(119, 70%, 50%)",
-          loc: 83975,
-        },
-        {
-          name: "movie",
-          color: "hsl(60, 70%, 50%)",
-          loc: 9695,
-        },
-        {
-          name: "user",
-          color: "hsl(310, 70%, 50%)",
-          loc: 27277,
-        },
-      ],
-    },
-    {
-      name: "Other Tools & Technologies",
+      name: "Backend Development",
       color: "hsl(39, 70%, 50%)",
       children: [
         {
-          name: "clone",
+          name: "Core Backend",
           color: "hsl(225, 70%, 50%)",
-          loc: 67771,
+          children: [
+            { name: "Node.js", color: "hsl(225, 70%, 50%)", loc: 78 },
+            { name: "Express", color: "hsl(337, 70%, 50%)", loc: 75 },
+          ],
         },
         {
-          name: "intersect",
-          color: "hsl(337, 70%, 50%)",
-          loc: 123524,
-        },
-        {
-          name: "merge",
+          name: "Databases",
           color: "hsl(251, 70%, 50%)",
-          loc: 91110,
-        },
-        {
-          name: "reverse",
-          color: "hsl(24, 70%, 50%)",
-          loc: 29608,
-        },
-        {
-          name: "toArray",
-          color: "hsl(217, 70%, 50%)",
-          loc: 177833,
-        },
-        {
-          name: "toObject",
-          color: "hsl(193, 70%, 50%)",
-          loc: 5546,
-        },
-        {
-          name: "fromCSV",
-          color: "hsl(93, 70%, 50%)",
-          loc: 48703,
-        },
-        {
-          name: "slice",
-          color: "hsl(294, 70%, 50%)",
-          loc: 148941,
-        },
-        {
-          name: "append",
-          color: "hsl(140, 70%, 50%)",
-          loc: 160378,
-        },
-        {
-          name: "prepend",
-          color: "hsl(34, 70%, 50%)",
-          loc: 113806,
-        },
-        {
-          name: "shuffle",
-          color: "hsl(157, 70%, 50%)",
-          loc: 53853,
-        },
-        {
-          name: "pick",
-          color: "hsl(84, 70%, 50%)",
-          loc: 148405,
-        },
-        {
-          name: "plouc",
-          color: "hsl(83, 70%, 50%)",
-          loc: 12546,
+          children: [
+            { name: "MongoDB", color: "hsl(251, 70%, 50%)", loc: 72 },
+            { name: "PostgreSQL", color: "hsl(24, 70%, 50%)", loc: 70 },
+          ],
         },
       ],
     },
     {
-      name: "General Skills",
+      name: "Mobile Development",
+      color: "hsl(60, 70%, 50%)",
+      children: [
+        { name: "React-Native", color: "hsl(310, 70%, 50%)", loc: 85 },
+      ],
+    },
+    {
+      name: "Data Visualization",
+      color: "hsl(139, 70%, 50%)",
+      children: [
+        { name: "D3.js", color: "hsl(35, 70%, 50%)", loc: 75 },
+        { name: "victory-charts", color: "hsl(255, 70%, 50%)", loc: 70 },
+        { name: "ant-charts", color: "hsl(119, 70%, 50%)", loc: 68 },
+      ],
+    },
+    {
+      name: "DevOps & Cloud",
+      color: "hsl(323, 70%, 50%)",
+      children: [
+        {
+          name: "Cloud Platforms",
+          color: "hsl(172, 70%, 50%)",
+          children: [
+            { name: "AWS", color: "hsl(172, 70%, 50%)", loc: 80 },
+            { name: "GCP", color: "hsl(33, 70%, 50%)", loc: 78 },
+          ],
+        },
+        {
+          name: "Tools",
+          color: "hsl(23, 70%, 50%)",
+          children: [
+            { name: "Docker", color: "hsl(23, 70%, 50%)", loc: 75 },
+            { name: "Terraform", color: "hsl(308, 70%, 50%)", loc: 70 },
+          ],
+        },
+      ],
+    },
+    {
+      name: "Testing & Automation",
       color: "hsl(312, 70%, 50%)",
       children: [
+        { name: "Jest", color: "hsl(233, 70%, 50%)", loc: 80 },
+        { name: "GitHub Actions", color: "hsl(111, 70%, 50%)", loc: 78 },
+      ],
+    },
+    {
+      name: "Design & Collaboration",
+      color: "hsl(217, 70%, 50%)",
+      children: [
         {
-          name: "trim",
-          color: "hsl(233, 70%, 50%)",
-          loc: 139141,
+          name: "Design Tools",
+          color: "hsl(193, 70%, 50%)",
+          children: [
+            { name: "Figma", color: "hsl(193, 70%, 50%)", loc: 95 },
+            { name: "Adobe XD", color: "hsl(93, 70%, 50%)", loc: 90 },
+          ],
         },
         {
-          name: "slugify",
-          color: "hsl(111, 70%, 50%)",
-          loc: 35499,
-        },
-        {
-          name: "snakeCase",
-          color: "hsl(268, 70%, 50%)",
-          loc: 150908,
-        },
-        {
-          name: "camelCase",
-          color: "hsl(164, 70%, 50%)",
-          loc: 33280,
-        },
-        {
-          name: "repeat",
-          color: "hsl(101, 70%, 50%)",
-          loc: 112933,
-        },
-        {
-          name: "padLeft",
-          color: "hsl(121, 70%, 50%)",
-          loc: 45969,
-        },
-        {
-          name: "padRight",
-          color: "hsl(240, 70%, 50%)",
-          loc: 172470,
-        },
-        {
-          name: "sanitize",
-          color: "hsl(60, 70%, 50%)",
-          loc: 164288,
-        },
-        {
-          name: "ploucify",
-          color: "hsl(131, 70%, 50%)",
-          loc: 173535,
+          name: "Collaboration Tools",
+          color: "hsl(276, 70%, 50%)",
+          children: [
+            { name: "Google Sheets", color: "hsl(320, 70%, 50%)", loc: 60 },
+            { name: "Git", color: "hsl(356, 70%, 50%)", loc: 85 },
+          ],
         },
       ],
     },
     {
-      name: "Hobbies",
-      color: "hsl(125, 70%, 50%)",
+      name: "Additional Skills",
+      color: "hsl(36, 70%, 50%)",
       children: [
-        {
-          name: "greetings",
-          color: "hsl(229, 70%, 50%)",
-          children: [
-            {
-              name: "hey",
-              color: "hsl(50, 70%, 50%)",
-              loc: 6913,
-            },
-            {
-              name: "HOWDY",
-              color: "hsl(290, 70%, 50%)",
-              loc: 11620,
-            },
-            {
-              name: "aloha",
-              color: "hsl(119, 70%, 50%)",
-              loc: 195941,
-            },
-            {
-              name: "AHOY",
-              color: "hsl(250, 70%, 50%)",
-              loc: 119058,
-            },
-          ],
-        },
-        {
-          name: "other",
-          color: "hsl(276, 70%, 50%)",
-          loc: 104059,
-        },
-        {
-          name: "path",
-          color: "hsl(320, 70%, 50%)",
-          children: [
-            {
-              name: "pathA",
-              color: "hsl(356, 70%, 50%)",
-              loc: 170532,
-            },
-            {
-              name: "pathB",
-              color: "hsl(139, 70%, 50%)",
-              children: [
-                {
-                  name: "pathB1",
-                  color: "hsl(287, 70%, 50%)",
-                  loc: 26500,
-                },
-                {
-                  name: "pathB2",
-                  color: "hsl(282, 70%, 50%)",
-                  loc: 125125,
-                },
-                {
-                  name: "pathB3",
-                  color: "hsl(29, 70%, 50%)",
-                  loc: 128225,
-                },
-                {
-                  name: "pathB4",
-                  color: "hsl(185, 70%, 50%)",
-                  loc: 154336,
-                },
-              ],
-            },
-            {
-              name: "pathC",
-              color: "hsl(323, 70%, 50%)",
-              children: [
-                {
-                  name: "pathC1",
-                  color: "hsl(205, 70%, 50%)",
-                  loc: 108929,
-                },
-                {
-                  name: "pathC2",
-                  color: "hsl(23, 70%, 50%)",
-                  loc: 144405,
-                },
-                {
-                  name: "pathC3",
-                  color: "hsl(308, 70%, 50%)",
-                  loc: 151869,
-                },
-                {
-                  name: "pathC4",
-                  color: "hsl(172, 70%, 50%)",
-                  loc: 135221,
-                },
-                {
-                  name: "pathC5",
-                  color: "hsl(33, 70%, 50%)",
-                  loc: 45569,
-                },
-                {
-                  name: "pathC6",
-                  color: "hsl(36, 70%, 50%)",
-                  loc: 103880,
-                },
-                {
-                  name: "pathC7",
-                  color: "hsl(313, 70%, 50%)",
-                  loc: 177941,
-                },
-                {
-                  name: "pathC8",
-                  color: "hsl(168, 70%, 50%)",
-                  loc: 199634,
-                },
-                {
-                  name: "pathC9",
-                  color: "hsl(25, 70%, 50%)",
-                  loc: 8249,
-                },
-              ],
-            },
-          ],
-        },
+        { name: "Jquery", color: "hsl(313, 70%, 50%)", loc: 50 },
+        { name: "Wordpress", color: "hsl(168, 70%, 50%)", loc: 55 },
+        { name: "Webflow", color: "hsl(25, 70%, 50%)", loc: 60 },
+        { name: "GIMP", color: "hsl(50, 70%, 50%)", loc: 58 },
+        { name: "ImageMagick", color: "hsl(250, 70%, 50%)", loc: 52 },
+        { name: "Entrepreneurship", color: "hsl(290, 70%, 50%)", loc: 65 },
+        { name: "Bitcoin", color: "hsl(119, 70%, 50%)", loc: 60 },
+        { name: "Java", color: "hsl(60, 70%, 50%)", loc: 70 },
+        { name: "C++", color: "hsl(310, 70%, 50%)", loc: 68 },
       ],
     },
   ],
