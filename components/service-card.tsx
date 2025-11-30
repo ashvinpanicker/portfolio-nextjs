e07e9f7b8734505e14b2fd6f006cf7d6ebec84c1
Ashvin Panicker <agnjkafgh@gmail.com>
2025-11-30 12:28:16 +0700

    change services animation

--- components/service-card.tsx
+++ components/service-card.tsx
@@ -7,32 +7,26 @@ import { ServiceProps } from "@/lib/types";
 const fadeInAnimationVariants = {
   initial: {
     opacity: 0,
-    y: 100,
+    y: 40,
   },
-  animate: (index: number) => ({
+  animate: {
     opacity: 1,
     y: 0,
     transition: {
-      delay: 0.05 * index,
+      duration: 0.5,
+      ease: "easeOut",
     },
-  }),
+  },
 };
 
 export default function ServiceCard({
   title,
   description,
   icon: Icon,
-  index,
-}: ServiceProps & { index: number }) {
+}: ServiceProps) {
   return (
     <motion.div
       variants={fadeInAnimationVariants}
-      initial="initial"
-      whileInView="animate"
-      viewport={{
-        once: true,
-      }}
-      custom={index}
       className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden p-5 hover:bg-gray-200 transition dark:text-white dark:bg-white/10 dark:hover:bg-white/20 flex flex-col items-center text-center"
     >
       <div className="mb-4">
--- components/services.tsx
+++ components/services.tsx
@@ -1,10 +1,20 @@
 "use client";
 
+import { motion } from "framer-motion";
 import React from "react";
 import SectionHeading from "./section-heading";
 import { servicesData } from "@/lib/data";
 import ServiceCard from "./service-card";
 import { useSectionInView } from "@/lib/hooks";
+
+const containerVariants = {
+  initial: {},
+  animate: {
+    transition: {
+      staggerChildren: 0.1,
+    },
+  },
+};
 
 export default function Services() {
   const { ref } = useSectionInView("Services", 0.8);
@@ -12,13 +22,21 @@ export default function Services() {
   return (
     <section ref={ref} id="services" className="scroll-mt-28 mb-28">
       <SectionHeading>Services</SectionHeading>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+      <motion.div
+        variants={containerVariants}
+        initial="initial"
+        whileInView="animate"
+        viewport={{
+          once: true,
+        }}
+        className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4"
+      >
         {servicesData.map((service, index) => (
           <React.Fragment key={index}>
-            <ServiceCard {...service} index={index} />
+            <ServiceCard {...service} />
           </React.Fragment>
         ))}
-      </div>
+      </motion.div>
     </section>
   );
 }
