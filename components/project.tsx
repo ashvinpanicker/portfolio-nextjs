f651d35b0c12497b04a67218232100d502740dd2
Ashvin Panicker <agnjkafgh@gmail.com>
2026-04-26 03:11:55 +0530

    fix: resolve mobile layout and interaction issues
    - Fix horizontal scroll on mobile by adding overflow-x: hidden to html
    
    - Change header breakpoints to lg to prevent clipping on iPad mini
    
    - Restrict project card hover animations to desktop (sm:) to prevent tilting on mobile scroll

--- components/project.tsx
+++ components/project.tsx
@@ -46,16 +46,16 @@ export default function Project({
             className="
                 block sm:absolute sm:top-8 sm:-right-40 w-full sm:w-[28.25rem] rounded-t-lg shadow-2xl
                 transition 
-                group-hover:scale-[1.04]
-                group-hover:-translate-x-3
-                group-hover:translate-y-3
-                group-hover:-rotate-2
+                sm:group-hover:scale-[1.04]
+                sm:group-hover:-translate-x-3
+                sm:group-hover:translate-y-3
+                sm:group-hover:-rotate-2
 
-                group-even:group-hover:translate-x-3
-                group-even:group-hover:translate-y-3
-                group-even:group-hover:rotate-2
+                sm:group-even:group-hover:translate-x-3
+                sm:group-even:group-hover:translate-y-3
+                sm:group-even:group-hover:rotate-2
 
-                group-even:right-[initial] group-even:-left-40
+                sm:group-even:right-[initial] sm:group-even:-left-40
             "
           />
 
